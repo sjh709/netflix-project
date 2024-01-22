ccdbeff6a7124ef29c29d8358bf7c7c9d0bd455b
Jihee <sjh709@naver.com>
2024-01-22 18:29:02 +0900

    라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetail from './pages/MovieDetail';
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movies' element={<Movies />} />
+      <Route path='/movie/:id' element={<MovieDetail />} />
+    </Routes>
+  );
 }
 
 export default App;
