21f05886d98814c3d5a1c2d21fa20c10da36137b
Jihee <sjh709@naver.com>
2024-01-22 19:07:37 +0900

    네비게이션바 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
 import MovieDetail from './pages/MovieDetail';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/movies' element={<Movies />} />
-      <Route path='/movie/:id' element={<MovieDetail />} />
-    </Routes>
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movie/:id' element={<MovieDetail />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <Navbar bg='dark' expand='lg'>
+      <Container fluid>
+        <Navbar.Brand>
+          <Link to='/'>
+            <img
+              width={95}
+              src='https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png'
+            />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav
+            className='me-auto my-2 my-lg-0'
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Link to='/' className='nav-item'>
+              Home
+            </Link>
+            <Link to='/movies' className='nav-item'>
+              Movies
+            </Link>
+          </Nav>
+          <Form className='d-flex'>
+            <Form.Control
+              type='search'
+              placeholder='Search'
+              className='me-2'
+              aria-label='Search'
+            />
+            <Button variant='outline-danger'>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
